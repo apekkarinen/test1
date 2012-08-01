7f8a32f8340aeb4fd101220f72b05211a0f6b5a8
ausr <ausr@mint-lappy1.(none)>
2012-08-01 20:26:34 +0300

    commit uusibranch

--- testi/src/testi/Testi.java
+++ testi/src/testi/Testi.java
@@ -6,5 +6,6 @@ public class Testi {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Testing");
+        System.out.println("Testing new branch");
     }
 }
